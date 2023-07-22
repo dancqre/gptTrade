a3b95af00002306bf0c99ab6ef410717e74f33f2
dancqre <94985901+dancqre@users.noreply.github.com>
2023-07-22 03:41:22 +0300

    Final version

---
+++ main.js
@@ -0,0 +1,77 @@
+//SLIDER
+
+const slider = document.querySelector('#slider');
+const sliderItems = Array.from(slider.children);
+const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
+
+sliderItems.forEach(function (slide, index){
+	if (index !== 1) slide.classList.add('none');
+
+	slide.dataset.index = index;
+
+	sliderItems[1].setAttribute('data-active', '');
+	slide.addEventListener('click', function(){
+
+		slide.classList.add('none');
+		slide.removeAttribute('data-active');
+		let nextSlideIndex; 
+
+		if (index + 1 === sliderItems.length){
+			nextSlideIndex = 1;
+		}else{
+			nextSlideIndex = index + 1;
+		}
+
+		const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);	
+		nextSlide.classList.remove('none');
+		nextSlide.setAttribute('data-active', '');
+
+	})
+})
+btnNext.onclick = function () {
+	const currentSlide = slider.querySelector('[data-active]');
+	const currentSlideIndex = +currentSlide.dataset.index;
+	
+	currentSlide.classList.add('none');
+	currentSlide.removeAttribute('data-active');
+
+	const nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 1 : currentSlideIndex + 1;
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('none');
+	nextSlide.setAttribute('data-active', '');
+}	
+btnPrev.onclick = function () {
+	const currentSlide = slider.querySelector('[data-active]');
+	const currentSlideIndex = +currentSlide.dataset.index;
+	currentSlide.classList.add('none');
+	currentSlide.removeAttribute('data-active');
+
+	const nextSlideIndex = currentSlideIndex  === 1 ? sliderItems.length - 1 : currentSlideIndex - 1;
+
+	const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
+	nextSlide.classList.remove('none');
+	nextSlide.setAttribute('data-active', '');
+}	
+
+//Nav
+
+const menuLinks = document.querySelectorAll('a[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+	function onMenuLinkClick(e){
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		} 
+	}
+}
